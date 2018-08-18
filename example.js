46ec01dc43188c86d3772a58b409c0e88a6c12ec
SerafinTech <jtserafin@gmail.com>
2018-08-18 11:31:20 -0400

    a

--- example.js
+++ example.js
@@ -2,9 +2,9 @@ const Apa102spi = require('apa102-spi')
 
 var n = 0
 
-led = new Apa102spi(24*8, 100)
+led = new Apa102spi(24*8, 256)
 
-led.setLedColor(24*8-1, 30, 255, 0, 0)
+led.setLedColor(24*8, 30, 255, 0, 0)
 
 led.sendLeds()
 

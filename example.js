3bc2ad900099050b1f68e9269f110bc66bf47052
SerafinTech <jtserafin@gmail.com>
2018-08-18 11:59:10 -0400

    a

--- example.js
+++ example.js
@@ -7,18 +7,18 @@ led = new Apa102spi(24*8, 100)
 setter()
 
 function setter() {
-  console.log(n)
+  
   led.setLedColor(n, 30, 0, 0, 255)
   if(n === 0) {
-    led.setLedColor((24*8)-1, 30, 0, 0, 0)
+    led.setLedColor((24*8)-1, 0, 0, 0, 0)
   } else {
     led.setLedColor(n - 1, 0, 0, 0, 0)
   }
-
+  console.log(n, led.writeBuffer[n], led.writeBuffer[n+1], led.writeBuffer[n+2], led.writeBuffer[n+3])
   n++
   if(n >= (24*8)) {
     n = 0
   }
   led.sendLeds()
-  setTimeout(() => {setter()}, 125)
+  setTimeout(() => {setter()}, 250)
 }

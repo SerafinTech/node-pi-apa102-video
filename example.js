fd70c79634674814d9806fb7d5d0bafaa731d129
SerafinTech <jtserafin@gmail.com>
2018-08-19 10:50:07 -0400

    a

--- example.js
+++ example.js
@@ -12,6 +12,6 @@ const matrix = [
   [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
 ]
 
-const player = new LedVideo(matrix, 24, 31)
+const player = new LedVideo(matrix, 100, 31)
 
 player.play(process.argv[2])
